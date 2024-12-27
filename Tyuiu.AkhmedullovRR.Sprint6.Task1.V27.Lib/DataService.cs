e00f99b4bec509995842309d09ff5a45c6288244
Равиль <fullbotrr@mail.ru>
2024-12-27 05:06:56 +0500

    kuvug

---
+++ Tyuiu.AkhmedullovRR.Sprint6.Task1.V27.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.AkhmedullovRR.Sprint6.Task1.V27.Lib
+{
+    public class DataService : ISprint6Task1V27
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                int z = (2 * x - 2);
+                if (z == 0)
+                {
+                    valueArray[count] = 0;
+                }
+                else
+                {
+                    y = ((4 - 2 * x) + ((2 + (Math.Cos(x))) / (2 * x - 2)));
+                    valueArray[count] = Math.Round(y, 2);
+                }
+                count++;
+            }
+            return valueArray;
+
+        }
+
+    }
+
+}
---
+++ Tyuiu.AkhmedullovRR.Sprint6.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.AkhmedullovRR.Sprint6.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
